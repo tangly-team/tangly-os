7224fe8c39939857ce84f75c31eb40990e43d723
marcelbaumann <marcel.baumann@tangly.net>
2020-06-26 16:19:17 +0200

    bug - correction of refactoring error

--- net.tangly.ports/src/test/java/net/tangly/crm/ports/CsvImportTest.java
+++ net.tangly.ports/src/test/java/net/tangly/crm/ports/CsvImportTest.java
@@ -1,36 +1,40 @@
 /*
  * Copyright 2006-2020 Marcel Baumann
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain
+ *  a copy of the License at
  *
  *          http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *   Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
+ *  under the License.
  */
 
-package net.tangly.crm.models;
+package net.tangly.crm.ports;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import net.tangly.bus.crm.CrmTags;
 import net.tangly.apps.Crm;
-import net.tangly.crm.ports.CrmCsvHdl;
+import net.tangly.bus.crm.CrmTags;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CsvImportTest {
+    static final String PACKAGE_NAME = "net/tangly/crm/ports/";
+
     @Test
     void testCsvCrm() throws IOException, URISyntaxException {
         Crm crm = new Crm();
         CrmTags.registerTags(crm.tagTypeRegistry());
         CrmCsvHdl handler = new CrmCsvHdl(crm);
 
-        handler.importNaturalEntities(fromResource("net/tangly/crm/models/naturalEntities.tsv"));
+        handler.importNaturalEntities(fromResource(PACKAGE_NAME + "naturalEntities.tsv"));
         assertThat(crm.naturalEntities().size()).isEqualTo(6);
 
         assertThat(crm.naturalEntities().get(0).oid()).isEqualTo(1);
@@ -40,13 +44,13 @@ public class CsvImportTest {
         assertThat(crm.naturalEntities().get(0).findPhoneNr(CrmTags.HOME)).isNotNull();
         // assertThat(crm.naturalEntities().getAt(0).findSite(CrmTags.HOME)).isNotNull();
 
-        handler.importLegalEntities(fromResource("net/tangly/crm/models/legalEntities.tsv"));
+        handler.importLegalEntities(fromResource(PACKAGE_NAME + "legalEntities.tsv"));
         assertThat(crm.legalEntities().size()).isEqualTo(3);
 
-        handler.importEmployees(fromResource("net/tangly/crm/models/employees.tsv"));
+        handler.importEmployees(fromResource(PACKAGE_NAME + "employees.tsv"));
         assertThat(crm.employees().size()).isEqualTo(5);
 
-        handler.importContracts(fromResource("net/tangly/crm/models/contracts.tsv"));
+        handler.importContracts(fromResource(PACKAGE_NAME + "contracts.tsv"));
         assertThat(crm.contracts().size()).isEqualTo(5);
     }
 
@@ -57,9 +61,9 @@ public class CsvImportTest {
         CrmTags.registerTags(crm.tagTypeRegistry());
         CrmCsvHdl handler = new CrmCsvHdl(crm);
 
-        handler.importNaturalEntities(fromResource("net/tangly/crm/models/naturalEntities.tsv"));
-        handler.importLegalEntities(fromResource("net/tangly/crm/models/legalEntities.tsv"));
-        handler.importEmployees(fromResource("net/tangly/crm/models/employees.tsv"));
+        handler.importNaturalEntities(fromResource(PACKAGE_NAME + "naturalEntities.tsv"));
+        handler.importLegalEntities(fromResource(PACKAGE_NAME + "legalEntities.tsv"));
+        handler.importEmployees(fromResource(PACKAGE_NAME + "employees.tsv"));
 
     }
 
