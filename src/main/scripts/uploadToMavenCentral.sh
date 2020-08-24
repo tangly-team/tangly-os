c38bd05f342325df87ae56cdcd572b12206ed13e
marcelbaumann <marcel.baumann@tangly.net>
2020-08-24 16:25:35 +0200

    chore - release tangly-os components 0.2.5

---
+++ src/main/scripts/uploadToMavenCentral.sh
@@ -0,0 +1,34 @@
+#
+# Copyright 2006-2020 Marcel Baumann
+#
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations
+#  under the License.
+#
+
+siteRootDir=/Users/Shared/Projects/tangly-os
+
+function upload() {
+  cd $1
+  gradle publishMavenJavaPublicationToMavenRepository -Pmode=prod
+  cd ..
+}
+
+# Uploads need to be sequential to avoid spurious errors on sonatype staging area
+cd $siteRootDir
+upload ./net.tangly.bdd
+upload ./net.tangly.bus
+upload ./net.tangly.commons
+upload ./net.tangly.dev
+upload ./net.tangly.fsm
+upload ./net.tangly.gleam
+upload ./net.tangly.orm
+upload ./net.tangly.ports
+
+
+
