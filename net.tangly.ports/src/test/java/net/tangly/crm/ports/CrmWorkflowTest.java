c74de90ecf7f9a8d082b058427d8552044d8ba3b
marcelbaumann <marcel.baumann@tangly.net>
2020-09-12 10:47:11 +0200

    feature - major update of static website with new theme docsy and all blogs due to quality deterioration of the blogger platform (font and layout changes on all older blogs).

--- net.tangly.ports/src/test/java/net/tangly/crm/ports/CrmWorkflowTest.java
+++ net.tangly.ports/src/test/java/net/tangly/crm/ports/CrmWorkflowTest.java
@@ -25,6 +25,7 @@ import net.tangly.bus.crm.CrmTags;
 import net.tangly.bus.crm.LegalEntity;
 import net.tangly.bus.crm.NaturalEntity;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -44,6 +45,7 @@ class CrmWorkflowTest {
     }
 
     @Test
+    @Tag("localTest")
     void testCompanyTsvCrm() {
         CrmWorkflows crmWorkflows = new CrmWorkflows(new Crm());
         crmWorkflows.importCrmEntities(Path.of("/Users/Shared/tangly"));
