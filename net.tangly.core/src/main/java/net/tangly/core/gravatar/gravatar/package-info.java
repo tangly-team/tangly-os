6af87f6bd9df4f068412014616b3a8b15b11d0fe
Marcel Baumann <marcelbaumann@gmail.com>
2023-02-18 20:19:35 +0100

    chore - refactor

---
+++ net.tangly.core/src/main/java/net/tangly/core/gravatar/gravatar/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2006-2021 Marcel Baumann
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+ * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+/**
+ * Support for retrieving <a href="https://en.gravatar.com/">gravatar</a> images is provided through this package. Gravatar images is an image associated with
+ * a natural entity and can be used in multiple sites.
+ */
+package net.tangly.core.gravatar.gravatar;
