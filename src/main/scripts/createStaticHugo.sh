0054cd7bd99028626c114c6c0310ac6650e11d7b
marcelbaumann <marcel.baumann@tangly.net>
2023-04-02 17:10:35 +0200

    feature - blog for April

---
+++ src/main/scripts/createStaticHugo.sh
@@ -0,0 +1,35 @@
+#
+# Copyright 202-2023 Marcel Baumann
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
+# OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+prjRootDir=/Users/Shared/Projects/
+prjName=tangly-os
+prjDir=$prjRootDir$prjName
+websiteStaticDir=$prjDir/src/site/website/static
+
+mkdir -p $websiteStaticDir/docs/bdd/api-bdd
+mkdir -p $websiteStaticDir/docs/commons/api-commons
+mkdir -p $websiteStaticDir/docs/dev/api-dev
+mkdir -p $websiteStaticDir/docs/fsm/api-fsm
+mkdir -p $websiteStaticDir/docs/gleam/api-gleam
+mkdir -p $websiteStaticDir/docs/ui/api-ui
+
+mkdir -p $websiteStaticDir/domains/agile/api-agile
+mkdir -p $websiteStaticDir/domains/apps/api-apps
+mkdir -p $websiteStaticDir/domains/collaborators/api-collaborators
+mkdir -p $websiteStaticDir/domains/crm/api-crm
+mkdir -p $websiteStaticDir/domains/invoices/api-invoices
+mkdir -p $websiteStaticDir/domains/ledger/api-ledger
+mkdir -p $websiteStaticDir/domains/products/api-products
+mkdir -p $websiteStaticDir/domains/shared/api-shared
+mkdir -p $websiteStaticDir/domains/ui/api-ui
+
+mkdir -p $websiteStaticDir/docs/ideas/agilearchitecture/agile-architecture-scene-images
+mkdir -p $websiteStaticDir/docs/ideas/architecturecodescene/architecture-code-scene-images
