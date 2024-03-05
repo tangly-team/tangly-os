22436c3bac6b3361b48e31818e3d06e4add0e3d6
Marcel Baumann <marcel.baumann@tangly.net>
2024-03-05 13:45:07 +0100

    bug - corrected regression bug due to new version of SO-chart

--- net.tangly.ui/src/main/java/net/tangly/ui/app/domain/AnalyticsView.java
+++ net.tangly.ui/src/main/java/net/tangly/ui/app/domain/AnalyticsView.java
@@ -1,13 +1,14 @@
 /*
- * Copyright 2006-2022 Marcel Baumann
+ * Copyright 2006-2024 Marcel Baumann
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of
  * the License at
  *
- *          https://apache.org/licenses/LICENSE-2.0
+ *          http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
  * OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ *
  */
 
 package net.tangly.ui.app.domain;
@@ -31,6 +32,8 @@ import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 
@@ -90,15 +93,16 @@ public abstract class AnalyticsView extends VerticalLayout {
 
     protected LineChart createLineChart(@NotNull String name, @NotNull DateData xValues, @NotNull BiFunction<LocalDate, LocalDate, BigDecimal> compute,
                                         @NotNull RectangularCoordinate rc) {
-        Data data = new Data();
-        data.setName(name);
         LocalDate start;
         LocalDate end = null;
+        List<BigDecimal> dataValues = new ArrayList<>();
         for (LocalDate date : xValues) {
             start = end;
             end = date;
-            data.add(compute.apply(start, end));
+            dataValues.add(compute.apply(start, end));
         }
+        Data data = new Data(dataValues.toArray(BigDecimal[]::new));
+        data.setName(name);
         LineChart lineChart = new LineChart(xValues, data);
         lineChart.setName(name);
         lineChart.plotOn(rc);
