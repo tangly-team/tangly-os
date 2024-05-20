1fe222991a39a06c6d4eee7a3fbca0d7329de4c9
Marcel Baumann <marcelbaumann@gmail.com>
2024-05-20 11:10:50 +0200

    test - integration tests support new entities in products domain

--- net.tangly.erp-integration/src/main/java/net/tangly/erp/ErpStore.java
+++ net.tangly.erp-integration/src/main/java/net/tangly/erp/ErpStore.java
@@ -101,6 +101,7 @@ public record ErpStore(@NotNull FileSystem fs) {
 
             var productsRoot = dataRoot().resolve(PRODUCTS);
             copy(PRODUCTS_PACKAGE_NAME, productsRoot, ProductsAdapter.PRODUCTS_TSV);
+            copy(PRODUCTS_PACKAGE_NAME, productsRoot, ProductsAdapter.WORK_CONTRACTS_TSV);
             copy(PRODUCTS_PACKAGE_NAME, productsRoot, ProductsAdapter.ASSIGNMENTS_TSV);
             copy(PRODUCTS_PACKAGE_NAME, productsRoot, ProductsAdapter.EFFORTS_TSV);
             copy(STR."\{PRODUCTS_PACKAGE_NAME}2020/", productsRoot.resolve("2020"), "2020-efforts.yaml");
