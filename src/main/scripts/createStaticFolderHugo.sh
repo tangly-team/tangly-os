9293ddeb6d9819cdc412bf1a531e5b9e8b3afdaf
Marcel Baumann <marcelbaumann@gmail.com>
2024-09-02 08:28:14 +0200

    feature - blog september

--- src/main/scripts/createStaticFolderHugo.sh
+++ src/main/scripts/createStaticFolderHugo.sh
@@ -4,7 +4,7 @@
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of
 # the License at
 #
-#          http://www.apache.org/licenses/LICENSE-2.0
+#          https://apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES
 # OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -19,8 +19,11 @@ prjName=tangly-os
 prjDir=$prjRootDir$prjName
 websiteStaticDir=$prjDir/src/site/website/static
 
+mkdir -p $websiteStaticDir/docs/PA/api-apps
 mkdir -p $websiteStaticDir/docs/bdd/api-bdd
+mkdir -p $websiteStaticDir/docs/XMS/api-cmd
 mkdir -p $websiteStaticDir/docs/commons/api-commons
+mkdir -p $websiteStaticDir/docs/core/api-core
 mkdir -p $websiteStaticDir/docs/dev/api-dev
 mkdir -p $websiteStaticDir/docs/fsm/api-fsm
 mkdir -p $websiteStaticDir/docs/gleam/api-gleam
@@ -28,13 +31,11 @@ mkdir -p $websiteStaticDir/docs/ui/api-ui
 mkdir -p $websiteStaticDir/docs/ui/api-web
 
 mkdir -p $websiteStaticDir/domains/agile/api-agile
-mkdir -p $websiteStaticDir/domains/apps/api-apps
 mkdir -p $websiteStaticDir/domains/collaborators/api-collaborators
 mkdir -p $websiteStaticDir/domains/crm/api-crm
 mkdir -p $websiteStaticDir/domains/invoices/api-invoices
 mkdir -p $websiteStaticDir/domains/ledger/api-ledger
 mkdir -p $websiteStaticDir/domains/products/api-products
-mkdir -p $websiteStaticDir/domains/shared/api-shared
 mkdir -p $websiteStaticDir/domains/ui/api-ui
 
 mkdir -p $websiteStaticDir/ideas/agilearchitecture/agile-architecture-scene-images
